5ae6b1f5bb96241babb24c5f60de09286cbea453
Rayiz3 <jusmint3@gmail.com>
2024-07-23 15:41:22 +0900

    create file : index.d.tsx

---
+++ stripeClient/src/index.d.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+type LanguageOptions = 'en' | 'ko'
+
+type ThemeOptions = 'dark'
+
+type DeviceOptions = 'mobile' | 'desktop'
+
+type PageOptions = 'home' | 'characters' | 'plans' | 'login' | 'help' | 'releases' | 'faq'
+
+type HelpPageTabOptions = 'faq' | 'releases'
