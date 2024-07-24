aec472b3030a14cd9ff98726c970aa64c6b287b9
Rayiz3 <jusmint3@gmail.com>
2024-07-24 09:51:15 +0900

    minor update

--- stripeClient/src/components/PaymentElement.tsx
+++ stripeClient/src/components/PaymentElement.tsx
@@ -9,16 +9,16 @@ export type PaymentElementProps = ElementProps<'payment', StripePaymentElementCh
 
 // payment method, card number, expiry, CVC and nation forms for payment
 export const PaymentElement: Component<PaymentElementProps> = (props) => {
-  const [wrapper, setWrapper] = createWrapper()
+    const [wrapper, setWrapper] = createWrapper()
 
-  createStripeElement(
-    wrapper,
-    'payment',
-    optionsPaymentElement,
-    (type, event) => props[type]?.(event),
-  );
+    createStripeElement(
+        wrapper,
+        'payment',
+        optionsPaymentElement,
+        (type, event) => props[type]?.(event),
+    );
 
-  (PaymentElement as any).__elementType = 'payment'
+    (PaymentElement as any).__elementType = 'payment'
 
-  return <div class={props.class} ref={setWrapper} />
+    return <div class={props.class} ref={setWrapper} />
 }
