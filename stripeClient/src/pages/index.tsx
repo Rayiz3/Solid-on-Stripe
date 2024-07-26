2fa387d2139a22c6426120bbfd5e6598754978a0
Rayiz3 <jusmint3@gmail.com>
2024-07-26 10:23:11 +0900

    add : SelectionPage, SubscriptionDashboardPage, SubscriptionCodePage

--- stripeClient/src/pages/index.tsx
+++ stripeClient/src/pages/index.tsx
@@ -1,13 +1,19 @@
 import { Route } from '@solidjs/router'
 
+import SelectionPage from './SelectionPage'
 import PaymentPage from './PaymentPage'
 import RedirectPage from './RedirectPage'
+import SubscriptionDashboardPage from './SubscriptionDashboardPage'
+import SubscriptionCodePage from './SubscriptionCodePage'
 
 const Pages = () => {
   return (
     <>
-      <Route path="/" component={PaymentPage} />
+      <Route path="/" component={SelectionPage} />
       <Route path="/redirection" component={RedirectPage} />
+      <Route path="/payment" component={PaymentPage} />
+      <Route path="/subscribe" component={SubscriptionDashboardPage} />
+      <Route path="/subscribeCode" component={SubscriptionCodePage} />
     </>
   )
 }
