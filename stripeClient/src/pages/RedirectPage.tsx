d0a47d99b7e1c50df882c42940286f3712ee58ba
Rayiz3 <jusmint3@gmail.com>
2024-07-26 16:19:06 +0900

    add : redirection from SubscriptionCodePage

--- stripeClient/src/pages/RedirectPage.tsx
+++ stripeClient/src/pages/RedirectPage.tsx
@@ -1,18 +1,38 @@
-import { Component } from 'solid-js'
+import type { Component } from 'solid-js'
+import { Match, Switch } from 'solid-js';
+
 import { style } from '@macaron-css/core';
+import { links } from '../property/Links';
+import { size } from '../property/Size';
+import { themeSys } from '../system/Theme';
+
+interface pageFromType {
+  pageFrom: 'payment' | 'subscriptionCode' | 'subscriptionDashboard'
+  sessionId?: string
+}
 
 const redirectMessage = style({
-    color: "rgb(105, 115, 134)",
+    color: themeSys.state.text,
     fontSize: "16px",
     lineHeight: "20px",
     paddingTop: "12px",
     textAlign: "center",
+
+    margin: size.space.section,
   })
 
-const RedirectPage: Component = () => {
+const RedirectPage: Component<pageFromType> = (props: pageFromType) => {
   return (
     <>
-        <div class={redirectMessage}>Payment succeeded!</div>
+      <div class={redirectMessage}>Payment succeeded!</div>
+      <Switch>
+        <Match when={props.pageFrom === 'subscriptionCode'}>
+          <form class="getPortal" action={links.serverAddress + "/create-portal-session"} method="post">
+            <input type="hidden" name="session_id" value={props.sessionId}/>
+            <button type="submit"> Manage your billing information </button>
+          </form>
+        </Match>
+      </Switch>
     </>
   )
 }
