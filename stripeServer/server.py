ea4bb7908c9a5c47c78b3b4f09a8fba2e88198a6
Rayiz3 <jusmint3@gmail.com>
2024-08-02 17:44:31 +0900

    add : fulfill_payment_intent(), refund_payment(), get_stripe_customer_id() add : - enable to get payment intent id from every purchase. - get customer id from DB. if not, create new customer.

--- stripeServer/server.py
+++ stripeServer/server.py
@@ -31,6 +31,10 @@ def calculate_order_amount(items): # items : { id: "..." } []
     # USD is two-decimal: 10$ == 1000
     return 1000
 
+def get_stripe_customer_id(vox_id):
+    # TODO: get corresponding stripe customer_id from DB
+    return ""
+
 # create payment : make one-time-pay intent
 @app.route('/create-payment-intent', methods=['POST'])
 def create_payment():
@@ -61,7 +65,16 @@ def create_checkout_session():
             lookup_keys=[request.form['lookup_key']],
             expand=['data.product']
         )
+
         pay_mode = request.form['pay_type']
+
+        customer_id = get_stripe_customer_id("voxfactory_id")
+        if not customer_id:
+            customer_id = stripe.Customer.create(
+                name="test_name",
+                email="test@email.com"
+            ).id
+            print("new customer:", customer_id)
 
         checkout_session = stripe.checkout.Session.create(
             line_items=[
@@ -71,7 +84,7 @@ def create_checkout_session():
                 },
             ],
             #ui_mode="embedded",
-            customer="cus_QJuqlN4E8qX64x",  # if blank in case of subscription & payment(customer_createion=always), it will create a new customer.
+            customer=customer_id,  # if blank in case of subscription & payment(customer_createion=always), it will create a new customer.
             mode=pay_mode,  # this is subscription or payment
             success_url=CLIENT_DOMAIN + '/subscribeCode?success=true&mode='+pay_mode+'&session_id={CHECKOUT_SESSION_ID}', # when payment success
             cancel_url=CLIENT_DOMAIN + '/subscribeCode?canceled=true', # when user go back to previous page
@@ -85,6 +98,7 @@ def create_checkout_session():
 # create portal session : make portal session for user
 @app.route('/create-portal-session', methods=['POST'])
 def create_portal_session():
+    try:
         # For demonstration purposes, we're using the Checkout session to retrieve the customer ID.
         # Typically this is stored alongside the authenticated user in your database.
         checkout_session_id = request.form.get('session_id')
@@ -95,8 +109,23 @@ def create_portal_session():
             return_url=CLIENT_DOMAIN,
         )
         return redirect(portalSession.url, code=303)
+    except Exception as e:
+        print(e)
+        return "Server error", 500
 
+@app.route('/refund-payment', methods=['POST'])
+def refund_payment():
+    try:
+        data = json.loads(request.data)
 
+        refund = stripe.Refund.create(
+            payment_intent=data['paymentIntent']
+        )
+    
+    except Exception as e:
+        return jsonify(error=str(e)), 403
+
+# fulfill checkout : get the event data from "checkout.session.completed"
 def fulfill_checkout(session_id):
     try:
         # Retrieve the Checkout Session from the API with line_items expanded
@@ -113,24 +142,39 @@ def fulfill_checkout(session_id):
         # Check the Checkout Session's payment_status property to determine if fulfillment should be peformed
         if checkout_session.payment_status != 'unpaid':
             return {
-                "status" : "success",
                 "mode" : checkout_session.mode,                     # payment | subscription
                 "transaction" : "purchase",                         # purchase | change | cancel
                 "customer_id" : checkout_session.customer,          # id assigned by Stripe server
-                "email" : checkout_session.customer_details.email,  # If provided, this value will be used when the Customer object is created
                 "date" : convert_timestamp(checkout_session.created),                                      # Measured in seconds since the Unix epoch (1970.01.01. 09:00:00 GMT+0900)
                 "date_expiry" : None,                
                 "products" : products,
-            }
-        else:
-            return {
-                "status": "failed",
             }
     
     except Exception as e:
         print(e)
         return "Server error", 500
+
+# fulfill payment intent : get the event data from "payment_intent.succeeded"
+def fulfill_payment_intent(session_id):
+    try:
+        payment_intent = stripe.PaymentIntent.retrieve(
+            session_id
+        )
+
+        return {
+   	        'mode': payment_intent.description if payment_intent.description else 'payment',  # 'payment' | 'Subscription creation' | 'Subscription update'
+   	        'customer_id': payment_intent.customer,  # it will be null if the user has not made any subscription before
+            'payment_intent': payment_intent.id,  # it is needed due to the refund method.
+            'date': convert_timestamp(payment_intent.created),
+            'price': payment_intent.amount,
+            'currency': payment_intent.currency,
+        }
+
+    except Exception as e:
+        print(e)
+        return "Server error", 500
     
+# fulfill subscription : get the event datat from "customer.subscription.update"
 def fulfill_subscription(session_id, transaction):
     try:
         date = 0 # date when the transaction was created
@@ -155,8 +199,6 @@ def fulfill_subscription(session_id, transaction):
             date = subscription.created
 
         return {
-            "status" : "success",
-            "mode" : "subscription",                # payment | subscription
             "transaction" : transaction,            # add | change | cancel
             "customer_id" : subscription.customer,  # id assigned by Stripe server
             "date" : convert_timestamp(date),
@@ -168,10 +210,13 @@ def fulfill_subscription(session_id, transaction):
         print(e)
         return "Server error", 500
     
-# Use the secret provided by Stripe CLI for local testing
-# or your webhook endpoint's secret.
+# Use the secret provided by Stripe CLI for local testing.
+#   $ .\stripe listen --forward-to localhost:4242/webhook
+#
+# On the live mode, use your webhook endpoint's secret.
 endpoint_secret = 'whsec_ef6b97d6c336879fd5efa57c46bd35a6243655d7c5fe7647c112f3efe448d4d4'
 
+# webhook view : call whenever the Stripe server make a transaction of payment or subscription
 @app.route('/webhook', methods=['POST'])
 def webhook_view():
   payload = request.get_data(as_text=True)
@@ -189,15 +234,33 @@ def webhook_view():
     # Invalid signature
     return jsonify(success=False), 400
   
-  # for each transaction, get information:
+  # if (event['type'] in ['checkout.session.completed', 'checkout.session.async_payment_succeeded']):
+  #   print(fulfill_checkout(event['data']['object']['id']))
+
+  ## purchase information ##
+  #
+  # for each purchase, the webhook gets the information:
   # {
-  # 	'status': 'success' | 'failed',
-  # 	'mode': 'payment' | 'subscription',
-  # 	'transaction': 'purchase' | 'add' | 'change' | 'cancel',
+  # 	'mode': 'payment' | 'Subscription creation' | 'Subscription update',
+  # 	'customer_id': string,
+  #     'payment_intent': string,  # it is needed due to the refund method.
+  #     'date': string,
+  #     'price': number,
+  #     'currency': string,
+  # }
+
+  if (event['type'] == 'payment_intent.succeeded'):
+    print(fulfill_payment_intent(event['data']['object']['id']))
+
+  ## subscription transaction information ##
+  #
+  # for each transaction, the webhook gets the information:
+  # {
+  # 	'transaction': 'add' | 'change' | 'cancel',
   # 	'customer_id': string,
   #     'email': string,
-  #     'date': datetime,
-  #     'date_expiry': datetime | None,
+  #     'date': string,
+  #     'date_expiry': string | None,
   #     'products': {
   #         'id': string,
   #         'price': number,
@@ -205,17 +268,21 @@ def webhook_view():
   #     }[]
   # }
 
-  if (event['type'] in ['checkout.session.completed', 'checkout.session.async_payment_succeeded']):
-    print(fulfill_checkout(event['data']['object']['id']))
-
   if (event['type'] == 'customer.subscription.updated'):
     subscription_id = event['data']['object']['id']
 
+    # change subscription
     if ("plan" in event['data']['previous_attributes'].keys()):
         print(fulfill_subscription(subscription_id, "change"))
+
+    # cancel subscription
     elif (event['data']['object']['canceled_at']):
+        # During cancelation, subscription.update is called twice: cancelation and cancelation feedback
+        # One of them should be blocked.
         if ("cancel_at" in event['data']['previous_attributes'].keys()):
             print(fulfill_subscription(subscription_id, "cancel"))
+
+    # add subscription
     else:
         print(fulfill_subscription(subscription_id, "add"))
 
